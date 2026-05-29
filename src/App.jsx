323ae55f793bdb1dc60f8d4d111de7b1d11c7593
Muhammad Tariq <tariqsohail1110@gmail.com>
2026-05-30 00:51:09 +0500

    feat: added tailwind, .env.sample, and modified src files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth.js';
+import { login, logout } from './store/authSlice.js'
+import { Footer, Header } from './components/index.js';
+import { Outlet } from 'react-router-dom';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>A blog app with appwrite</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}));
+      } else {
+        dispatch(logout());
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-sc flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
