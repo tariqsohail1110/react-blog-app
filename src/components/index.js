3f86b2fba529050855d8b45c40bf9717710928bc
Muhammad Tariq <tariqsohail1110@gmail.com>
2026-05-31 03:50:46 +0500

    feat: enhance Header and Footer components with navigation and styling

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,9 @@
 import Header from "./Header/Header.jsx";
 import Footer from "./Footer/Footer.jsx";
+import Container from './Container/Container.jsx';
+import Logo from "./Logo.jsx";
+import LogoutBtn from "./Header/LogoutBtn.jsx";
 
 export {
-    Header, Footer
+    Header, Footer, Container, Logo, LogoutBtn
 }
