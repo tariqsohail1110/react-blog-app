5406bf5e7590b0de912ffdae47faf737c8803ad2
Muhammad Tariq <tariqsohail1110@gmail.com>
2026-06-01 00:17:25 +0500

    feat: add Rich Text Editor component and integrate it into PostForm

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,8 @@ import Footer from "./Footer/Footer.jsx";
 import Container from './Container/Container.jsx';
 import Logo from "./Logo.jsx";
 import LogoutBtn from "./Header/LogoutBtn.jsx";
+import RTE from "./RTE.jsx";
 
 export {
-    Header, Footer, Container, Logo, LogoutBtn
+    Header, Footer, Container, Logo, LogoutBtn, RTE
 }
